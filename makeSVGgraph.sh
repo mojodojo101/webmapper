2436bf0a8eb6ff8f8b904c6e434c51c9bf044195
root <malte.luhmann@yahoo.de>
2020-01-12 13:39:51 -0500

    added some color support

--- makeSVGgraph.sh
+++ makeSVGgraph.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
-usage="$1 <urlfile> <target> [-i == ignoreCase]"
-echo "$1 $2 $3"
+# https://www.graphviz.org/doc/info/attrs.html check this out if you want to change attributes
+#might want to change the way the args are parsed this is dones pretty lazy here or just give this entire thing an overhaul^^
+usage="$1 <urlfile> <target> [-i == ignoreCase] [importantURIsFile]"
+nodes=""
+header='graph Site {\ngraph [overlap=false\n rankdir ="LR"\noutputMode="edgesfirst"\n ranksep = "1.2 equally"\nbgcolor="darkseagreen"\n];\n'
+footer='}'
+nodestyle=' [style=filled] [fillcolor="deepskyblue3"]'
+importantNodestyle=' [style=filled] [fillcolor="red"]'
+masterNodestyle=' [style=filled] [fillcolor="coral3"]'
+
+if [ $# -eq 4 ];then
+	important=`cat $4`
+else
+	important=""
+fi
 if [ $# -lt 2 ];then
 	echo $usage
 	exit 2
@@ -11,18 +24,21 @@ else
 	urls=`cat $1 |sed "s/ (.*)//g" | tr '[:upper:]' '[:lower:]' | sort | uniq`;
 fi
 hostname=`echo $urls | head -1 |  cut -d'/' -f1-3`
-nodes=""
-header='graph Site {\ngraph [overlap=false\n rankdir ="LR"\noutputMode="edgesfirst"\n ranksep = "1.2 equally"\n];\n'
-footer='
-}
-'
-masternode='"'$hostname'"'' [URL="'$hostname'"];\n'
+
+masternode='"'$hostname'"'' [URL="'$hostname'"]'$masterNodestyle';\n'
 make_node()
 {
 nodeurl=$1
 nodelabel=`echo $nodeurl | grep -o "/[^/]*$"`
 parent=`echo $nodeurl | rev | cut -d '/' -f 2- | rev` 
-nodes=$nodes'"'$nodeurl'" [label="'$nodelabel'"]'' [URL="'$nodeurl'"];\n"'$parent'" -- "'$nodeurl'";\n'
+nodetext='"'$nodeurl'" [label="'$nodelabel'"]'' [URL="'$nodeurl'"]'$nodestyle';\n"'$parent'" -- "'$nodeurl'";\n'
+for imp in $important;
+do
+	if [ "$imp" = "$nodeurl" ];then
+		nodetext='"'$nodeurl'" [label="'$nodelabel'"]'' [URL="'$nodeurl'"]'$importantNodestyle';\n"'$parent'" -- "'$nodeurl'";\n'
+	fi;
+done
+nodes=$nodes$nodetext
 }
 
 for url in $urls;
